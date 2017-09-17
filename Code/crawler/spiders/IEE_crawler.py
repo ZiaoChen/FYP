36fdf5b73377515d08330ab8dfe48d73bcf0486f
ZiaoChen <466930995@qq.com>
2017-09-17 20:54:48 +0800

    Crawler Files

---
+++ Code/crawler/spiders/IEE_crawler.py
@@ -0,0 +1,19 @@
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+from scrapy import Request
+
+
+class IEEESpider(Spider):
+    name = "ieeespider"
+    allow_domain = ["ieee.org"]
+
+    def start_requests(self):
+        total_page = 10
+        base_url = "http://ieeexplore.ieee.org/search/searchresult.jsp?queryText=machine%20"
+        base_url2= "learning&pageNumber=%s&rowsPerPage=10"
+        for page in range(total_page):
+            print base_url + base_url2 % str(page+1)
+            yield Request(base_url + base_url2 % str(page+1), self.parse)
+
+    def parse(self, response):
+        sel = Selector(response)
