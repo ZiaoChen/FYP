70d6b25b6222ca51de31ee09006bd5ee9f17ec28
ZiaoChen <466930995@qq.com>
2017-09-29 00:42:01 +0800

    LDA jupyter

--- Code/ieee_spider_selenium.py
+++ Code/ieee_spider_selenium.py
@@ -6,19 +6,30 @@ import signal
 path = os.path.dirname(os.path.realpath(__file__))
 
 
-def get_paper_details(url):
-    driver = webdriver.Chrome("chromedriver")
+def get_paper_details(url, driver):
+
     driver.get(url)
     paper = {}
-    paper["Title"] = driver.find_element_by_tag_name("h1").text
-    paper["Abstract"] = driver.find_element_by_xpath("//div[contains(@class,'abstract-text') and contains(@class, 'ng-binding')]").text.encode('utf-8')
-    driver.find_element_by_xpath('//*[contains(text(),"Keywords")]').click()
-    paper["Keywords"] = driver.find_element_by_xpath("//ul[contains(@class,'u-mt-1')]").text
+    try:
+        paper["Title"] = driver.find_element_by_tag_name("h1").text
+    except:
+        paper["Title"] = None
+    try:
+        paper["Abstract"] = driver.find_element_by_xpath("//div[contains(@class,'abstract-text') and contains(@class, 'ng-binding')]").text.encode('utf-8')
+    except:
+        paper["Abstract"] = None
+    try:
+        driver.find_element_by_xpath('//*[contains(text(),"Keywords")]').click()
+        paper["Keywords"] = driver.find_element_by_xpath("//ul[contains(@class,'u-mt-1')]").text
+    except:
+        paper["Keywords"] = None
     print paper["Title"]
     return paper
     driver.quit()
-
-driver = webdriver.PhantomJS()
+driver = webdriver.Chrome("chromedriver")
+# driver = webdriver.PhantomJS()
+paper_driver = webdriver.PhantomJS()
+# paper_driver = webdriver.Chrome("chromedriver")
 base_url = "http://ieeexplore.ieee.org/search/searchresult.jsp?queryText=machine%20"
 base_url2 = "learning&pageNumber=%s&rowsPerPage=%s"
 total_page = 10
@@ -36,8 +47,9 @@ for page in range(total_page):
     for paper_element in paper_url_list:
         paper_url = paper_element.find_element_by_tag_name("a").get_attribute("href")
         print paper_url
-        writer.writerow(get_paper_details(paper_url))
+        writer.writerow(get_paper_details(paper_url, paper_driver))
 
 driver.service.process.send_signal(signal.SIGTERM)
 driver.quit()
+paper_driver.quit()
 
