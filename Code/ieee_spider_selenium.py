0b758a5e194dbd0dd7c1915da3173713dd4af3cc
ZiaoChen <466930995@qq.com>
2017-10-08 10:55:20 +0800

    LDA

--- Code/ieee_spider_selenium.py
+++ Code/ieee_spider_selenium.py
@@ -4,16 +4,13 @@ import os
 import csv
 import signal
 path = os.path.dirname(os.path.realpath(__file__))
-
+from selenium.webdriver import ChromeOptions
 
 def get_paper_details(url, driver):
 
     driver.get(url)
     paper = {}
-    try:
-        paper["Title"] = driver.find_element_by_tag_name("h1").text
-    except:
-        paper["Title"] = None
+    paper["Title"] = driver.find_element_by_tag_name("h1").text
     try:
         paper["Abstract"] = driver.find_element_by_xpath("//div[contains(@class,'abstract-text') and contains(@class, 'ng-binding')]").text.encode('utf-8')
     except:
@@ -26,7 +23,10 @@ def get_paper_details(url, driver):
     print paper["Title"]
     return paper
     driver.quit()
-driver = webdriver.Chrome("chromedriver")
+options = ChromeOptions()
+options.add_argument('--disable-popup-blocking')
+options.add_experimental_option("prefs", {'profile.managed_default_content_settings.images': 2})
+driver = webdriver.Chrome("chromedriver", chrome_options=options)
 # driver = webdriver.PhantomJS()
 paper_driver = webdriver.PhantomJS()
 # paper_driver = webdriver.Chrome("chromedriver")
@@ -42,8 +42,12 @@ for page in range(total_page):
     driver.get(base_url + base_url2 % (str(page + 1), paper_per_page))
     for _ in range(int(paper_per_page / 10)):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(0.1)
     paper_url_list = driver.find_elements_by_xpath("//div[contains(@class,'List-results-items') and contains(@class, 'ng-scope')]")
+    while not paper_url_list:
+        time.sleep(1)
+        print "Page not loaded, trying again..."
+        paper_url_list = driver.find_elements_by_xpath(
+            "//div[contains(@class,'List-results-items') and contains(@class, 'ng-scope')]")
     for paper_element in paper_url_list:
         paper_url = paper_element.find_element_by_tag_name("a").get_attribute("href")
         print paper_url
